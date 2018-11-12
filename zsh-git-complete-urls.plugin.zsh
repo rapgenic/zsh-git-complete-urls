77e4d10357c2b3bfd2f0c415276cfcc57ca0c300
Giulio Girardi <giulio@rapgenic.com>
2018-11-12 14:27:35 +0100

    Clipboard completions are displayed first

--- zsh-git-complete-urls.plugin.zsh
+++ zsh-git-complete-urls.plugin.zsh
@@ -1,4 +1,4 @@
-# If clippaste function is present, adds to git clone completions 
+# If clippaste function is present, adds to git clone completions
 # a git url present in the clipboard
 
 __git_remote_clipboard() {
@@ -9,21 +9,21 @@ __git_remote_clipboard() {
 
     # Enable PCRE regexs
     setopt RE_MATCH_PCRE
-    
-    # If clippaste does not exist or returns an error (missing 
+
+    # If clippaste does not exist or returns an error (missing
     # xclip and xsel) return silently
     local clipboard && clipboard=$(clippaste) || return
 
     # regex adapted from https://github.com/jonschlinkert/is-git-url
     if [[ "$clipboard" =~ "(?:git|ssh|https?|git@[-\w.]+):(\/\/)?(\S*?\.git|github.com\S*?)(\/?|\#[-\d\w._]+?)$" ]]; then
-        _wanted remote-clipboard expl 'remote urls' compadd $clipboard
+        _wanted remote-clipboard expl 'remote urls' compadd -2 -V 1 -- "$clipboard"
     fi
 }
 
 __git_any_repositories () {
     _alternative \
+        'remote-clipboard::__git_remote_clipboard' \
         'local-repositories::__git_local_repositories' \
         'remotes: :__git_remotes' \
-        'remote-repositories::__git_remote_repositories' \
-        'remote-clipboard::__git_remote_clipboard'
+        'remote-repositories::__git_remote_repositories'
 }
